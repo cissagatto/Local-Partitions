98edc310525e1a19ed733714c546cc3d7895d1d2
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2025-02-07 21:22:29 -0300

    Add files via upload

--- Utils/local.py
+++ Utils/local.py
@@ -1,9 +1,57 @@
+##############################################################################
+# LOCAL PARTITIONS MULTI-LABEL CLASSIFICATION                                #
+# Copyright (C) 2025                                                         #
+#                                                                            #
+# This code is free software: you can redistribute it and/or modify it under #
+# the terms of the GNU General Public License as published by the Free       #
+# Software Foundation, either version 3 of the License, or (at your option)  #
+# any later version. This code is distributed in the hope that it will be    #
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of     #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General   #
+# Public License for more details.                                           #
+#                                                                            #
+# 1 - Prof PhD Elaine Cecilia Gatto                                          #
+# 2 - Prof PhD Ricardo Cerri                                                 #
+# 3 - Prof PhD Mauri Ferrandin                                               #
+# 4 - Prof PhD Celine Vens                                                   #
+# 5 - PhD Felipe Nakano Kenji                                                #
+# 6 - Prof PhD Jesse Read                                                    #
+#                                                                            #
+# 1 = Federal University of São Carlos - UFSCar - https://www2.ufscar.br     #
+# Campus São Carlos | Computer Department - DC - https://site.dc.ufscar.br | #
+# Post Graduate Program in Computer Science - PPGCC                          # 
+# http://ppgcc.dc.ufscar.br | Bioinformatics and Machine Learning Group      #
+# BIOMAL - http://www.biomal.ufscar.br                                       # 
+#                                                                            # 
+# 1 = Federal University of Lavras - UFLA                                    #
+#                                                                            # 
+# 2 = State University of São Paulo - USP                                    #
+#                                                                            # 
+# 3 - Federal University of Santa Catarina Campus Blumenau - UFSC            #
+# https://ufsc.br/                                                           #
+#                                                                            #
+# 4 and 5 - Katholieke Universiteit Leuven Campus Kulak Kortrijk Belgium     #
+# Medicine Department - https://kulak.kuleuven.be/                           #
+# https://kulak.kuleuven.be/nl/over_kulak/faculteiten/geneeskunde            #
+#                                                                            #
+# 6 - Ecole Polytechnique | Institut Polytechnique de Paris | 1 rue Honoré   #
+# d’Estienne d’Orves - 91120 - Palaiseau - FRANCE                            #
+#                                                                            #
+##############################################################################
+
+
+
+
 import sys
+import os
+import pickle
+from joblib import dump
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier  
 from sklearn.metrics import average_precision_score
 from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.metrics import average_precision_score
 
 if __name__ == '__main__':
     
@@ -39,8 +87,10 @@ if __name__ == '__main__':
     baseModel = RandomForestClassifier(n_estimators = n_estimators, random_state = random_state)
     classifier = BinaryRelevance(baseModel)
     classifier.fit(X_train, Y_train)
-    binary_predictions = classifier.predict(X_test)
-    probabilities = classifier.predict_proba(X_test)
+    binary_predictions = classifier.predict(X_test.values)
+    probabilities = classifier.predict_proba(X_test.values)
+    
+    # preds = clf.predict(X.values) 
     
     true = (diretorio + "/y_true.csv")
     pred = (diretorio + "/y_pred_bin.csv")
@@ -57,3 +107,43 @@ if __name__ == '__main__':
     probabilities_2 = pd.DataFrame(probabilities_2)
     probabilities_2.columns = labels_y_test
     probabilities_2.to_csv(proba, index=False)
+    
+      
+    # List to store model size information
+    model_sizes = []
+    
+    # Save the model using pickle
+    model_path = (diretorio + "/model_rf.pkl")
+    with open(model_path, "wb") as f:
+        pickle.dump(classifier, f)
+    
+    # Get the file size
+    file_size_bytes = os.path.getsize(model_path)  # in bytes
+    file_size_kb = file_size_bytes / 1024  # convert to KB
+    file_size_mb = file_size_kb / 1024  # convert to MB
+    
+    # Store in the list
+    model_sizes.append(["pickle", file_size_bytes, file_size_kb, file_size_mb])
+    
+    print(f"Pickle model size: {file_size_mb:.2f} MB")
+    
+    # Save the model using joblib
+    model_path = (diretorio + "/model_rf.joblib")
+    dump(classifier, model_path)
+    
+    # Get the file size
+    file_size_bytes = os.path.getsize(model_path)  # in bytes
+    file_size_kb = file_size_bytes / 1024  # convert to KB
+    file_size_mb = file_size_kb / 1024  # convert to MB
+    
+    # Store in the list
+    model_sizes.append(["joblib", file_size_bytes, file_size_kb, file_size_mb])
+    
+    print(f"Joblib model size: {file_size_mb:.2f} MB")
+    
+    # Create a DataFrame and save to a single CSV
+    name_csv = (diretorio + "/model_sizes.csv")
+    df_size = pd.DataFrame(model_sizes, columns=["Format", "Size (Bytes)", "Size (KB)", "Size (MB)"])
+    df_size.to_csv(name_csv, index=False)
+    
+    print("Model sizes saved to 'model_sizes.csv'")
